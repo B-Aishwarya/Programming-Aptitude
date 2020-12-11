415e853b902a2721c2393a393522b921391af93f
B-Aishwarya <72915098+B-Aishwarya@users.noreply.github.com>
2020-12-11 19:48:13 +0530

    Patterns

---
+++ Patterns/PatternTwo.java
@@ -0,0 +1,16 @@
+package com.xworkz.Patterns;
+
+public class PatternTwo {
+	public static void main(String[] args)
+	{
+		for(int i=0;i<=4; i++)
+		{
+			for(int j=4; j>=i+1; j--)
+			{
+				System.out.println("");
+			
+		}
+	}
+
+}
+}
