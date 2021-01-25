b9706cc62e2df1a342479302b430a77062d3ea93
B-Aishwarya <72915098+B-Aishwarya@users.noreply.github.com>
2021-01-25 08:41:44 +0530

    Add files via upload

---
+++ pgmApti/StaticBlock.java
@@ -0,0 +1,16 @@
+package com.xworkz.pgmApti;
+
+public class StaticBlock {
+	static int i; 
+    int j;  
+    static { 
+        i = 10; 
+        System.out.println("static block called "); 
+    } 
+    
+    
+    public static void main(String[] args) {
+		System.out.println(StaticBlock.i);
+	}
+
+}
